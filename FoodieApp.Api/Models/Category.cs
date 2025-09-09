08a4f1b0c75c6b3ae2df3d2e11be3398639c4668
LagsKing <jakubkrol@onet.com.pl>
2025-09-09 12:20:13 +0200

    fix: project structure

--- FoodieApp.Api/Models/Category.cs
+++
@@ -1,9 +0,0 @@
-namespace FoodieApp.Api.Models
-{
-    public class Category
-    {
-        public int Id { get; set; }
-        public string Name { get; set; } = null!;
-        public ICollection<Item> Items { get; set; } = new List<Item>();
-    }
-}
